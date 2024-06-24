63d3f762610a86dd65a7b1f31d7db585bbec9434
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2024-06-24 16:34:32 -0300

    Ejercicio Extra

--- src/Ejercicio3.jsx
+++ src/Ejercicio3.jsx
@@ -2,21 +2,15 @@ import React from 'react'
 import { useState } from "react"
 import ElementoLista from './ElementoLista';
 export default function Ejercicio3() {
-
+  const [materias,setMaterias]=useState(["Informatica Aplicada II","Programacion II","Robotica","Matemática","Física"])
   return (
     <div>
       <h2>Ejercicio 3: Modularización</h2>
       <p>Algunas materias de quinto año:</p>
    
       <ul>
-   
-      <ElementoLista materia={"Informatica Aplicada II"}></ElementoLista>
-      <ElementoLista materia={"Programacion II"}></ElementoLista>
-      <ElementoLista materia={"Robotica"}></ElementoLista>
-      <ElementoLista materia={"Matemática"}></ElementoLista>
-      <ElementoLista materia={"Física"}></ElementoLista>
+      {materias.map((materia)=><ElementoLista materia={materia}></ElementoLista>)}
       </ul>
     </div>
   )
 }
-{materias.map((materia)=><ElementoLista materia={materia}></ElementoLista>)}
